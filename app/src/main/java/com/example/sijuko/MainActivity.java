6e977fe0781f4aca320ea0e86498139e33236b95
pynkaaryani <83490753+pynkaaryani@users.noreply.github.com>
2022-04-25 22:06:31 +0700

    bottom navigation

--- app/src/main/java/com/example/sijuko/MainActivity.java
+++ app/src/main/java/com/example/sijuko/MainActivity.java
@@ -1,37 +1,53 @@
 package com.example.sijuko;
 
-import android.os.Bundle;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-import com.example.sijuko.databinding.ActivityMainBinding;
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.sijuko.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
+    BottomNavigationView bottomNavigationView;
+
+    com.example.sijuko.HomeFragment homeFragment = new com.example.sijuko.HomeFragment();
+    com.example.sijuko.HistoryFragment historyFragment = new com.example.sijuko.HistoryFragment();
+    com.example.sijuko.NotificationFragment notificationFragment = new com.example.sijuko.NotificationFragment();
+    com.example.sijuko.ProfileFragment profileFragment = new com.example.sijuko.ProfileFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        bottomNavigationView = findViewById(R.id.battomNav);
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+                        return true;
+                    case R.id.history:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,historyFragment).commit();
+                        return true;
+                    case R.id.notification:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,notificationFragment).commit();
+                        return true;
+                    case R.id.profil:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,profileFragment).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
     }
-
 }
