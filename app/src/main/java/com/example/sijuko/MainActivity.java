055b78d6f05aa964e65322084eb5c9f91b0fce6b
Ervan Chodry <ervanchodry112@gmail.com>
2022-04-28 18:19:21 +0700

    Menambah beberapa komponen

--- app/src/main/java/com/example/sijuko/MainActivity.java
+++ app/src/main/java/com/example/sijuko/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.sijuko;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -11,10 +13,16 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.sijuko.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    com.example.sijuko.HomeFragment homeFragment = new com.example.sijuko.HomeFragment();
+    com.example.sijuko.HistoryFragment historyFragment = new com.example.sijuko.HistoryFragment();
+    com.example.sijuko.NotificationFragment notificationFragment = new com.example.sijuko.NotificationFragment();
+    com.example.sijuko.ProfileFragment profileFragment = new com.example.sijuko.ProfileFragment();
+    com.example.sijuko.ScannerFragment scannerFragment = new com.example.sijuko.ScannerFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +40,35 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.battomNav);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+                        return true;
+                    case R.id.history:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,historyFragment).commit();
+                        return true;
+                    case R.id.scanner:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,scannerFragment).commit();
+                        return true;
+                    case R.id.notification:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,notificationFragment).commit();
+                        return true;
+                    case R.id.profil:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,profileFragment).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 
 }
