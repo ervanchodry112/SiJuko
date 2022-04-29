588a7c9393a5bb37b9424edf7fb9d9eea56e2bcc
Ervan Chodry <ervanchodry112@gmail.com>
2022-04-29 18:27:23 +0700

    Menambahkan Recycler Article View di fragment_home.xml

---
+++ app/src/main/java/com/example/sijuko/Article.java
@@ -0,0 +1,30 @@
+package com.example.sijuko;
+
+public class Article {
+
+    private String date, judul, author;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
--- app/src/main/java/com/example/sijuko/MainActivity.java
+++ app/src/main/java/com/example/sijuko/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.sijuko;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
@@ -14,16 +15,18 @@ import com.example.sijuko.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    BottomNavigationView bottomNavigationView;
-    ActivityMainBinding binding;
+    private BottomNavigationView bottomNavigationView;
+    private ActivityMainBinding binding;
 
-    com.example.sijuko.HomeFragment homeFragment = new com.example.sijuko.HomeFragment();
-    com.example.sijuko.HistoryFragment historyFragment = new com.example.sijuko.HistoryFragment();
-    com.example.sijuko.ScannerFragment scannerFragment = new com.example.sijuko.ScannerFragment();
-    com.example.sijuko.NotificationFragment notificationFragment = new com.example.sijuko.NotificationFragment();
-    com.example.sijuko.ProfileFragment profileFragment = new com.example.sijuko.ProfileFragment();
+    private com.example.sijuko.HomeFragment homeFragment = new com.example.sijuko.HomeFragment();
+    private com.example.sijuko.HistoryFragment historyFragment = new com.example.sijuko.HistoryFragment();
+    private com.example.sijuko.ScannerFragment scannerFragment = new com.example.sijuko.ScannerFragment();
+    private com.example.sijuko.NotificationFragment notificationFragment = new com.example.sijuko.NotificationFragment();
+    private com.example.sijuko.ProfileFragment profileFragment = new com.example.sijuko.ProfileFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
